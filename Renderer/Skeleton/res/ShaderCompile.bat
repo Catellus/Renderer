6e9bf5af29497d6a559314743e0c5ad8f1296a4b
catellus <thecatellus@gmail.com>
2020-10-05 11:39:47 -0500

    Fixed ShaderCompile.bat Vulkan version/location

--- Renderer/Skeleton/res/ShaderCompile.bat
+++ Renderer/Skeleton/res/ShaderCompile.bat
@@ -1,7 +1,15 @@
 @echo off
-C:\VulkanSDK\1.2.135.0\Bin32\glslc.exe default.vert -o vert.spv
-echo Vert compile successful
-C:\VulkanSDK\1.2.135.0\Bin32\glslc.exe default.frag -o frag.spv
-echo Frag compile successful
+if EXIST C:\VulkanSDK\1.2.135.0\ (
+	C:\VulkanSDK\1.2.135.0\Bin32\glslc.exe default.vert -o vert.spv
+	echo Vert compile successful
+	C:\VulkanSDK\1.2.135.0\Bin32\glslc.exe default.frag -o frag.spv
+	echo Frag compile successful
+)
+if EXIST D:\VulkanSDK\1.2.148.1\ (
+	D:\VulkanSDK\1.2.148.1\Bin32\glslc.exe default.vert -o vert.spv
+	echo Vert compile successful
+	D:\VulkanSDK\1.2.148.1\Bin32\glslc.exe default.frag -o frag.spv
+	echo Frag compile successful
+)
 pause
 
